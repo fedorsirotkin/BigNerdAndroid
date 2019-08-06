2c249bf8e6c5b938e2b28219484c63e78b36090a
sirotkin-fa <fedorsirotkin@mail.ru>
2019-08-06 14:13:36 +0300

    Добавлены материалы шестой главы

--- geoquiz/src/main/java/ru/fedorsirotkin/geoquiz/QuizActivity.java
+++ geoquiz/src/main/java/ru/fedorsirotkin/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package ru.fedorsirotkin.geoquiz;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
 
-public class QuizActivity extends Activity {
+public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
